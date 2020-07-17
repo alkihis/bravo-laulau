15ae45b8a18d88621b812faad783f449381527c1
Alkihis <alkihis@gmail.com>
2020-07-17 22:38:50 +0200

    Added set

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Twitter from 'twitter-lite';
+import { Status, FullUser } from 'twitter-d';
 import { TWITTER_KEYS } from './constants';
 
 const client = new Twitter({
@@ -6,26 +7,35 @@ const client = new Twitter({
 });
 
 const seen = new Set<string>();
+const startup_time = Date.now();
 
 async function main() {
   const stream = client.stream('statuses/filter', {
     follow: '18896800'
   });
 
-  stream.on('data', tweet => {
+  stream.on('data', (tweet: Status & { text: string }) => {
     if (!('text' in tweet) || !('id_str' in tweet))
       return; // Probably a delete update
 
     if (seen.has(tweet.id_str))
       return; // Tweet already computed
 
+    const date = new Date(tweet.created_at).getTime();
+
+    if (date < startup_time)
+      return; // Tweet sent before bot is woken up
+
+    // Add tweet in seen set
     seen.add(tweet.id_str);
 
     if (tweet.text.startsWith('@'))
       return; // This is a reply
 
+    const screen_name = (<FullUser>tweet.user)?.screen_name ?? 'auvergnerhalpes';
+
     client.post('statuses/update', {
-      status: 'BRAVO LOLO !! ' + ('https://twitter.com/auvergnerhalpes/status/' + tweet.id_str)
+      status: 'BRAVO LOLO !! https://twitter.com/' + screen_name  + '/status/' + tweet.id_str
     });
   });
 
