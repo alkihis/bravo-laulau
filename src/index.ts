467a99c583f724ad70f173495f7fadd7ade9c447
Alkihis <alkihis@gmail.com>
2020-07-18 12:37:30 +0200

    Removed useless async function

--- src/index.ts
+++ src/index.ts
@@ -9,43 +9,38 @@ const client = new Twitter({
 const seen = new Set<string>();
 const startup_time = Date.now();
 
-async function main() {
-  const stream = client.stream('statuses/filter', {
-    follow: '18896800'
-  });
-
-  stream.on('data', (tweet: Status & { text: string }) => {
-    if (!('text' in tweet) || !('id_str' in tweet))
-      return; // Probably a delete update
-
-    if (seen.has(tweet.id_str))
-      return; // Tweet already computed
-
-    const date = new Date(tweet.created_at).getTime();
-
-    if (date < startup_time)
-      return; // Tweet sent before bot is woken up
-
-    // Add tweet in seen set
-    seen.add(tweet.id_str);
-
-    if (tweet.text.startsWith('@'))
-      return; // This is a reply
-
-    const screen_name = (<FullUser>tweet.user)?.screen_name ?? 'auvergnerhalpes';
-
-    client.post('statuses/update', {
-      status: 'BRAVO LAULAU !! https://twitter.com/' + screen_name  + '/status/' + tweet.id_str
-    });
-    console.log('Status', tweet.id_str, 'credited.');
-  });
-
-  console.log('Listening twitter stream.');
-}
-
-
-main().catch(e => {
-  console.log('Error', e);
+const stream = client.stream('statuses/filter', {
+  follow: '18896800'
 });
+
+stream.on('data', (tweet: Status & { text: string }) => {
+  if (!('text' in tweet) || !('id_str' in tweet))
+    return; // Probably a delete update
+
+  if (seen.has(tweet.id_str))
+    return; // Tweet already computed
+
+  const date = new Date(tweet.created_at).getTime();
+
+  if (date < startup_time)
+    return; // Tweet sent before bot is woken up
+
+  // Add tweet in seen set
+  seen.add(tweet.id_str);
+
+  if (tweet.text.startsWith('@'))
+    return; // This is a reply
+
+  const screen_name = (<FullUser>tweet.user)?.screen_name ?? 'auvergnerhalpes';
+
+  client
+    .post('statuses/update', {
+      status: 'BRAVO LAULAU !! https://twitter.com/' + screen_name  + '/status/' + tweet.id_str
+    })
+    .then(() => console.log('Status', tweet.id_str, 'credited.'))
+    .catch(e => console.error('Unable to post tweet.', e));
+});
+
+console.log('Listening twitter stream.');
 
 process.on('unhandledRejection', error => console.error("Unhandled rejection: ", error));
